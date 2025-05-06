196dafdd38d7b292d898a9563def5fee95b31365
GeiOoo <throw3n@gmail.com>
2025-05-06 10:23:19 +0200

    layout

--- renderer/components/AppCard.tsx
+++ renderer/components/AppCard.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { Delete, Edit, Pause, PlayArrow } from '@mui/icons-material';
-import { Box, Button, Card, CardActions, CardContent, CardHeader, IconButton, Skeleton, Stack, Typography } from '@mui/material';
+import { Box, Button, Card, CardActions, CardHeader, IconButton, Skeleton, Typography } from '@mui/material';
 import { useQuery } from '@tanstack/react-query';
 import Image from 'next/image';
 
@@ -16,40 +16,28 @@ export default function AppCard({ path, onDeleteApp }: {
     path: string;
     onDeleteApp: (path: string) => void;
 }) {
-    const { data, isLoading } = useQuery<AppData>({
+    const { data, isLoading } = useQuery({
         queryKey: ['appDetails', path],
         queryFn: async () => await window.ipc.getAppDetails(path),
         refetchOnWindowFocus: false,
         refetchInterval: 500,
     });
 
-    if (isLoading) {
-        return <Stack component={Card} p={1}>
-            <Stack direction='row' gap={2}>
-                <Skeleton variant='circular' width={40} height={40} />
-                <Skeleton width={150} height={40} />
-            </Stack>
-            <Skeleton height={40} />
-        </Stack>;
-    }
-
-    if (!data) {
-        return <Card><CardContent>Failed</CardContent></Card>;
-    }
+    const skeletonLengthInPixel = path.split('\\').pop()?.length * 8 || 0;
 
     return (
-        <Card key={data.path} raised={data.isRunning}>
+        <Card key={path} raised={data?.isRunning}>
             <CardHeader
-                action={<IconButton sx={{ ml: 1 }}><Edit /></IconButton>}
-                avatar={<Image alt='icon' src={data.icon.toDataURL()} height={32} width={32} />}
-                title={<Typography color={data.isRunning ? 'primary' : 'textPrimary'}>{data.name}</Typography>}
+                action={!isLoading && <IconButton sx={{ ml: 1 }}><Edit /></IconButton>}
+                avatar={isLoading ? <Skeleton variant='circular' width={32} height={32} /> : <Image alt='icon' src={data.icon.toDataURL()} height={32} width={32} />}
+                title={isLoading ? <Skeleton width={skeletonLengthInPixel} height={32} /> : <Typography color={data.isRunning ? 'primary' : 'textPrimary'}>{data.name}</Typography>}
             />
             <CardActions>
                 <Button size='small' color='error' startIcon={<Delete />} onClick={handleDelete} >
                     Delete
                 </Button>
                 <Box sx={{ flexGrow: 1 }} />
-                {data.isRunning ?
+                {data?.isRunning ?
                     <Button
                         size='small'
                         color='primary'
