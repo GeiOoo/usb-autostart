be6526ec6da2a7f24848fe653bcd76a46706b672
GeiOoo <throw3n@gmail.com>
2025-05-06 17:17:47 +0200

    Layout

--- renderer/components/AppCard.tsx
+++ renderer/components/AppCard.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { Delete, PlayArrow, Settings, Stop } from '@mui/icons-material';
-import { Button, Card, CardActions, CardHeader, IconButton, Skeleton, Typography } from '@mui/material';
+import { Button, Card, CardActions, CardHeader, IconButton, Skeleton, Stack, Tooltip, Typography } from '@mui/material';
 import { useQuery } from '@tanstack/react-query';
 import Image from 'next/image';
 
@@ -27,12 +27,29 @@ export default function AppCard({ path, onDeleteApp }: {
     const pathSskeletonLengthInPixel = path.length * 7 || 0;
 
     return (
-        <Card key={path} raised={data?.isRunning}>
+        <Stack component={Card} key={path} raised={data?.isRunning}>
             <CardHeader
                 action={<IconButton size='small' color='error' onClick={handleDelete} ><Delete /></IconButton>}
                 avatar={isLoading ? <Skeleton variant='circular' width={32} height={32} /> : <Image alt='icon' src={data.icon.toDataURL()} height={32} width={32} />}
                 title={isLoading ? <Skeleton width={nameSkeletonLengthInPixel} height={28} /> : <Typography color={data.isRunning ? 'primary' : 'textPrimary'}>{data.name}</Typography>}
-                subheader={isLoading ? <Skeleton width={pathSskeletonLengthInPixel} height={20} /> : <Typography fontFamily={'monospace'} color='textSecondary' variant='caption'>{path}</Typography>}
+                subheader={isLoading ?
+                    <Skeleton width={pathSskeletonLengthInPixel} height={20} />
+                    :
+                    <Tooltip title={path} placement='top' arrow>
+                        <Typography
+                            display={'block'}
+                            width={250}
+                            textOverflow={'ellipsis'}
+                            overflow={'hidden'}
+                            whiteSpace={'nowrap'}
+                            fontFamily={'monospace'}
+                            color='textSecondary'
+                            variant='caption'
+                        >
+                            {path}
+                        </Typography>
+                    </Tooltip>
+                }
             />
             <CardActions sx={{ pt: 0 }}>
                 <Button
@@ -52,7 +69,7 @@ export default function AppCard({ path, onDeleteApp }: {
                 />
                 <IconButton><Settings /></IconButton>
             </CardActions>
-        </Card >
+        </Stack >
     );
 
     function handleDelete() {
