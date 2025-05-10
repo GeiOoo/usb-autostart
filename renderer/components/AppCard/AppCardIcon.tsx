4d2f6341ce187408adb6565bfaa8c1e79c3ca7a5
GeiOoo <throw3n@gmail.com>
2025-05-10 08:59:56 +0200

    Compact View

---
+++ renderer/components/AppCard/AppCardIcon.tsx
@@ -0,0 +1,25 @@
+import { Card, Skeleton, Stack, useTheme } from '@mui/material';
+import { useQuery } from '@tanstack/react-query';
+import Image from 'next/image';
+
+export default function AppCardIcon({ path }: { path: string; }) {
+    const theme = useTheme();
+    const { data: processData, isLoading } = useQuery({
+        queryKey: ['appDetails', path],
+        queryFn: async () => await window.ipc.getAppDetails(path),
+        refetchOnWindowFocus: false,
+        refetchInterval: 1000,
+        select: data => {
+            return {
+                ...data,
+                icon: data.icon.toDataURL()
+            };
+        }
+    });
+
+    return (
+        <Stack component={Card} p={1} variant='outlined' borderColor={processData.isRunning ? `${theme.palette.success.main}99` : undefined} >
+            {isLoading ? <Skeleton variant='circular' width={16} height={16} /> : <Image alt='icon' src={processData.icon} height={16} width={16} />}
+        </Stack>
+    );
+}
--- renderer/components/Viewport.tsx
+++ renderer/components/Viewport.tsx
@@ -1,33 +1,52 @@
-import { Add, PlayArrow, Stop } from '@mui/icons-material';
-import { Button, Stack } from '@mui/material';
+import { Add, ArrowDropDown, ArrowDropUp, PlayArrow, Stop } from '@mui/icons-material';
+import { Button, Collapse, IconButton, Paper, Stack } from '@mui/material';
+import { useState } from 'react';
 import useLocalStorageState from '../hooks/useLocalStorageState';
 import AppCard, { AppMetaData } from './AppCard/AppCard';
+import AppCardIcon from './AppCard/AppCardIcon';
 import UsbSelect from './UsbSelect';
 
 export default function Viewport() {
     const [appDataList, setAppDataList] = useLocalStorageState<AppMetaData[]>([], 'appDataList');
+    const [expanded, setExpanded] = useState(false);
 
     return (
-        <Stack gap={2} padding={2} height={'100vh'}>
-            <Stack direction="row" gap={2}>
-                <Button size='small' startIcon={<Add />} variant='outlined' onClick={handleFileSelect}>Add App</Button>
-                <UsbSelect onSelectedUsbConnected={handleStartAll} onSelectedUsbDisconnected={handleStopAll} />
-                <Button size='small' startIcon={<PlayArrow />} variant='outlined' color="primary" onClick={handleStartAll}>Start All</Button>
-                <Button size='small' startIcon={<Stop />} variant='outlined' color="error" onClick={handleStopAll}>Stop All</Button>
+        <Stack height={'100vh'} >
+            <Stack component={Paper} padding={2} gap={2}>
+                <Stack direction="row" gap={2}>
+                    <IconButton onClick={() => setExpanded(prev => !prev)} sx={{ alignSelf: 'center' }}>
+                        {expanded ? <ArrowDropUp /> : <ArrowDropDown />}
+                    </IconButton>
+                    <Button size='small' startIcon={<Add />} variant='outlined' onClick={handleFileSelect}>Add App</Button>
+                    <UsbSelect onSelectedUsbConnected={handleStartAll} onSelectedUsbDisconnected={handleStopAll} />
+                    <Button size='small' startIcon={<PlayArrow />} variant='outlined' color="primary" onClick={handleStartAll}>Start All</Button>
+                    <Button size='small' startIcon={<Stop />} variant='outlined' color="error" onClick={handleStopAll}>Stop All</Button>
+                </Stack>
+                <Collapse in={!expanded}>
+                    <Stack direction={'row'} gap={1} flexWrap="wrap">
+                        {appDataList.map(data => (
+                            <AppCardIcon path={data.path} key={data.path} />
+                        ))}
+                    </Stack>
+                </Collapse>
             </Stack>
-            <Stack overflow={'auto'} direction={'row'} gap={2} flexWrap="wrap" alignItems={'baseline'}>
-                {appDataList
-                    .toSorted((a, b) => a.name.localeCompare(b.name))
-                    .map(data => (
-                        <AppCard
-                            key={data.path}
-                            data={data}
-                            onDeleteApp={handleDeleteApp}
-                            onUpdateAppMetaData={(oldPath, newData) => {
-                                setAppDataList(prev => prev.map(app => app.path === oldPath ? newData : app));
-                            }}
-                        />
-                    ))}
+            <Stack p={2}>
+                <Collapse in={expanded} >
+                    <Stack overflow={'auto'} direction={'row'} gap={2} flexWrap="wrap" alignItems={'baseline'}>
+                        {appDataList
+                            .toSorted((a, b) => a.name.localeCompare(b.name))
+                            .map(data => (
+                                <AppCard
+                                    key={data.path}
+                                    data={data}
+                                    onDeleteApp={handleDeleteApp}
+                                    onUpdateAppMetaData={(oldPath, newData) => {
+                                        setAppDataList(prev => prev.map(app => app.path === oldPath ? newData : app));
+                                    }}
+                                />
+                            ))}
+                    </Stack>
+                </Collapse>
             </Stack>
         </Stack>
     );
